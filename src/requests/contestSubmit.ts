eb322576e348bbeac2e3309d94acf35b3b87ca64
DESKTOP-HU69O7Q\tuanchill <tuanlt203@gmail.com>
2024-10-20 17:36:01 +0700

    fecth data and add search bar in ket qua cuoc thi

--- src/requests/contestSubmit.ts
+++ src/requests/contestSubmit.ts
@@ -60,16 +60,21 @@ export const createContestSubmission = async (contestSubmission: DataContestSubm
 };
 
 export const getContestSubmissionPagination = async (page: number, limit: number, keyword: string, tag: string) => {
-    const response = await tekdojoAxios.get(`${BASE_URL}/contest-submissions?populate[contest_entry][populate]=user&populate=thumbnail&pagination[page]=${page}&pagination[pageSize]=${limit}&sort[0]=id:asc&filters[contest_entry][candidateNumber][$containsi]=${keyword}&filters[tags][$containsi]=${tag}`); 
+    const response = await tekdojoAxios.get(`${BASE_URL}/contest-submissions?populate[contest_entry][populate]=user&populate=thumbnail&pagination[page]=${page}&pagination[pageSize]=${limit}&sort[0]=id:asc&filters[contest_entry][candidateNumber][$containsi]=${keyword}&filters[tags][$containsi]=${tag}`);
     return response.data;
 };
 
 export const getOneContestSubmission = async (id: string) => {
     const response = await tekdojoAxios.get(`${BASE_URL}/contest-submissions/${id}?populate[contest_entry][populate]=user&populate[thumbnail]=*&populate[assets]=*&populate[source]=*`);
-    return response.data ;
+    return response.data;
 }
 
 export const getContestSubmissionByContestEntry = async (contestEntryId: string) => {
     const response = await tekdojoAxios.get(`${BASE_URL}/contest-submissions?populate[contest_entry][populate]=user&populate=thumbnail&populate[assets]=*&filters[contest_entry][id][$eq]=${contestEntryId}`);
     return response.data;
 }
+
+export const getContestSubmissionResult = async (page: number, limit: number, name: string, candidateNumber: string, project: string) => {
+    const response = await tekdojoAxios.get(`${BASE_URL}/contest-submissions?populate[contest_entry][populate]=user&pagination[page]=${page}&pagination[pageSize]=${limit}&sort[1]=id:asc&sort[0]=QualifiedExam:asc&filters[contest_entry][candidateNumber][$containsi]=${candidateNumber}&filters[contest_entry][user][fullName][$containsi]=${name}&filters[title][$containsi]=${project}`); 
+    return response.data;
+};
